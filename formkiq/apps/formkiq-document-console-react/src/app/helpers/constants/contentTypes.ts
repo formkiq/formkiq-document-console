b5b6e60f8dcddb0891f7e3bb1faf3bb7a3cd91df
Regan Wolfrom <regan.wolfrom@gmail.com>
2023-02-23 19:08:53 -0600

    adjusted inline content type list (no Office docs and added others)

--- formkiq/apps/formkiq-document-console-react/src/app/helpers/constants/contentTypes.ts
+++ formkiq/apps/formkiq-document-console-react/src/app/helpers/constants/contentTypes.ts
@@ -22,29 +22,23 @@ export const OnlyOfficeNewFileExtensions: string[] = [
 ]
 
 export const InlineViewableContentTypes: string[] = [
+  'application/json',
   'application/pdf',
+  'audio/mpeg',
+  'audio/mp4',
+  'audio/ogg',
+  'audio/wav',
   'image/png',
   'image/gif',
   'image/jpg',
   'image/jpeg',
+  'image/svg+xml',
+  'image/vnd.microsoft.icon',
+  'image/webp',
   'text/plain',
-  'application/rtf',
-  'audio/mp4',
   'video/mp4',
-  'application/msword',
-  'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
-  'application/vnd.openxmlformats-officedocument.wordprocessingml.template',
-  'application/vnd.ms-word.document.macroEnabled.12',
-  'application/vnd.oasis.opendocument.text',
-  'application/vnd.ms-excel',
-  'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-  'application/vnd.openxmlformats-officedocument.spreadsheetml.template',
-  'application/vnd.oasis.opendocument.spreadsheet',
-  'application/vnd.ms-powerpoint',
-  'application/vnd.openxmlformats-officedocument.presentationml.presentation',
-  'application/vnd.openxmlformats-officedocument.presentationml.slideshow',
-  'application/vnd.openxmlformats-officedocument.presentationml.template',
-  'application/vnd.oasis.opendocument.presentation'
+  'video/ogg',
+  'video/webm',
 ]
 
 export const OcrContentTypes: string[] = [
