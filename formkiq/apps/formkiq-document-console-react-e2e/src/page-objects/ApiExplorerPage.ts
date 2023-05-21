dc156e1d10b0cd715c63ae14cdf0073c87fda5e7
Alainna Halliday <alainnahalliday@gmail.com>
2023-05-21 09:30:17 -0400

    Add documents api explorer integration tests

--- formkiq/apps/formkiq-document-console-react-e2e/src/page-objects/ApiExplorerPage.ts
+++ formkiq/apps/formkiq-document-console-react-e2e/src/page-objects/ApiExplorerPage.ts
@@ -4,21 +4,58 @@ export class ApiExplorerPage {
   readonly page: Page;
   readonly navigateLink: Locator;
   readonly integrationDropdown: Locator;
-  readonly systemSelect: Locator;
+  readonly fetchButton: Locator;
 
   constructor(page: Page) {
     this.page = page;
     this.integrationDropdown = page.getByTestId('expand-integrations');
     this.navigateLink = page.getByTestId('nav-api-explorer');
-    this.systemSelect = page.getByTestId('system-subfolder-select');
+    this.fetchButton = page.getByTestId('apiItem-fetch');
   }
 
   async openPage() {
     await this.integrationDropdown.click();
     await this.navigateLink.click();
   }
+}
 
-  async selectSystemFolder(folder: 'My Documents' | 'Team Documents') {
-    await this.systemSelect.selectOption(folder);
+export class ApiItem {
+  readonly page: Page;
+  readonly path: string;
+  readonly fetchButton: Locator;
+  readonly responseStatus: Locator;
+  readonly responseData: Locator;
+  readonly httpButton: Locator;
+  readonly curlButton: Locator;
+  readonly curlRequest: Locator;
+
+  constructor(page: Page, apiMethod: string, apiPath: string) {
+    this.page = page;
+    this.path = `apiItem-${apiMethod}${apiPath}`;
+    this.fetchButton = page.getByTestId(this.path).getByTestId('apiItem-fetch');
+    this.responseStatus = page
+      .getByTestId(this.path)
+      .getByTestId('apiItem-response-status');
+    this.responseData = page
+      .getByTestId(this.path)
+      .getByTestId('apiItem-response-data');
+
+    this.httpButton = page.getByTestId(this.path).getByTestId('apiItem-HTTP');
+    this.curlButton = page.getByTestId(this.path).getByTestId('apiItem-cURL');
+    this.curlRequest = page
+      .getByTestId(this.path)
+      .getByTestId('apiItem-curl-request');
+  }
+
+  async setCurl() {
+    await this.curlButton.click();
+  }
+
+  async open() {
+    await this.page.getByTestId(this.path).click();
+  }
+
+  async fetch() {
+    await this.fetchButton.click();
   }
 }
