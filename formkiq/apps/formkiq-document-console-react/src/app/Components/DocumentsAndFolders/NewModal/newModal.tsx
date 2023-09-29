d53bd06b578dc96729d46448e827ebd4e42e8016
Regan Wolfrom <regan.wolfrom@gmail.com>
2023-09-28 20:17:20 -0500

    Removed "Outbound Webhook" from New Modal

--- formkiq/apps/formkiq-document-console-react/src/app/Components/DocumentsAndFolders/NewModal/newModal.tsx
+++ formkiq/apps/formkiq-document-console-react/src/app/Components/DocumentsAndFolders/NewModal/newModal.tsx
@@ -302,7 +302,7 @@ export default function NewModal({
                         itemToCreate === 'outbound-webhook'
                           ? 'bg-gray-100 font-semibold border-gray-600'
                           : 'cursor-pointer hover:bg-gray-100'
-                      } mx-1 w-48 border-2 rounded-md flex flex-wrap justify-center p-2`}
+                      } mx-1 w-48 border-2 rounded-md flex flex-wrap justify-center p-2 hidden`}
                       onClick={(event) => (window.location.href = '/workflows')}
                     >
                       <div className="w-full h-12 text-gray-600 my-5 flex justify-center">
