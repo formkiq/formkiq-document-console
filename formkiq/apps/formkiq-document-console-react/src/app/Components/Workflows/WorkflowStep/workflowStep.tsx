a4be7e5552dd406912531efe2be8a2d0899cbea4
Regan Wolfrom <regan.wolfrom@gmail.com>
2023-11-20 15:03:53 -0600

    WIP designer and steps

--- formkiq/apps/formkiq-document-console-react/src/app/Components/Workflows/WorkflowStep/workflowStep.tsx
+++ formkiq/apps/formkiq-document-console-react/src/app/Components/Workflows/WorkflowStep/workflowStep.tsx
@@ -1,22 +1,63 @@
-import { useDrag } from 'react-dnd';
+import React, { useRef } from 'react';
+import {
+  DragSourceMonitor,
+  DropTargetMonitor,
+  useDrag,
+  useDrop,
+} from 'react-dnd';
 
-type Props = {
+interface WorkflowStepProps {
   id: string;
+  index: number;
   name: string;
+  moveStep: (dragIndex: number, hoverIndex: number) => void;
+}
+
+interface DragItem {
   type: string;
-};
+  id: string;
+  index: number;
+}
 
-const WorkflowStep = ({ id, name, type }: Props) => {
-  const [{ isDragging }, drag] = useDrag(() => ({
+const WorkflowStep: React.FC<WorkflowStepProps> = ({
+  id,
+  index,
+  name,
+  moveStep,
+}) => {
+  const ref = useRef<HTMLDivElement>(null);
+
+  const [, drop] = useDrop<DragItem, void, void>({
+    accept: 'STEP',
+    hover(item, monitor: DropTargetMonitor) {
+      if (!ref.current) {
+        return;
+      }
+      const dragIndex = item.index;
+      const hoverIndex = index;
+      if (dragIndex === hoverIndex) {
+        return;
+      }
+      moveStep(dragIndex, hoverIndex);
+      item.index = hoverIndex;
+    },
+  });
+
+  const [{ isDragging }, drag] = useDrag({
     type: 'STEP',
-    item: { id, type },
-    collect: (monitor) => ({
-      isDragging: !!monitor.isDragging(),
+    item: () => {
+      return { id, index };
+    },
+    collect: (monitor: DragSourceMonitor) => ({
+      isDragging: monitor.isDragging(),
     }),
-  }));
+  });
 
+  drag(drop(ref));
+
+  const opacity = isDragging ? 0 : 1;
   return (
-    <div ref={drag} style={{ opacity: isDragging ? 0.5 : 1 }}>
+    <div ref={ref} style={{ opacity }}>
       {name}
     </div>
   );
--- formkiq/apps/formkiq-document-console-react/src/app/Views/Workflows/workflowDesigner.tsx
+++ formkiq/apps/formkiq-document-console-react/src/app/Views/Workflows/workflowDesigner.tsx
@@ -1,13 +1,31 @@
+import { useCallback, useState } from 'react';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import { Helmet } from 'react-helmet-async';
 import WorkflowStep from '../../Components/Workflows/WorkflowStep/workflowStep';
 
+interface Step {
+  id: string;
+  name: string;
+}
+
 export function WorkflowDesigner() {
-  const steps = [
-    { id: '1', name: 'Start', type: 'start' },
-    // More entities...
-  ];
+  const [steps, setSteps] = useState<Step[]>([
+    { id: 'step1', name: 'Step 1' },
+    { id: 'step2', name: 'Step 2' },
+    // ... more steps
+  ]);
+
+  const moveStep = useCallback(
+    (dragIndex: number, hoverIndex: number) => {
+      const dragStep = steps[dragIndex];
+      const newSteps = [...steps];
+      newSteps.splice(dragIndex, 1);
+      newSteps.splice(hoverIndex, 0, dragStep);
+      setSteps(newSteps);
+    },
+    [steps]
+  );
 
   return (
     <>
@@ -16,8 +34,14 @@ export function WorkflowDesigner() {
       </Helmet>
       <DndProvider backend={HTML5Backend}>
         <div>
-          {steps.map((step) => (
-            <WorkflowStep key={step.id} {...step} />
+          {steps.map((step: Step, index: number) => (
+            <WorkflowStep
+              key={step.id}
+              id={step.id}
+              index={index}
+              name={step.name}
+              moveStep={moveStep}
+            />
           ))}
         </div>
       </DndProvider>
