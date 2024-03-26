2a2f03ad475837f1a2049eca8d2ed20eba0e622c
AnastasiiaPirus <a.pirus2000@gmail.com>
2024-03-26 12:15:14 -0400

    Fixed tags table paddings

--- formkiq/apps/formkiq-document-console-react/src/app/Components/TagSchemas/tables/compositeKeysTable.tsx
+++ formkiq/apps/formkiq-document-console-react/src/app/Components/TagSchemas/tables/compositeKeysTable.tsx
@@ -11,9 +11,9 @@ function CompositeKeysTable({compositeKeys}: CompositeKeysTableProps) {
         className="w-full bg-neutral-100 z-10 pt-2 border-b border-t text-transparent font-bold text-left border-neutral-300">
       <tr>
         <th
-          className=" w-6 border-b border-t p-4 pl-8 py-3 bg-clip-text bg-gradient-to-l from-primary-500 via-secondary-500 to-primary-600"></th>
+          className=" w-6 border-b border-t p-4 py-3 bg-clip-text bg-gradient-to-l from-primary-500 via-secondary-500 to-primary-600"></th>
         <th
-          className=" w-full  border-b border-t p-4 pl-8 py-3 bg-clip-text bg-gradient-to-l from-primary-500 via-secondary-500 to-primary-600">Values
+          className=" w-full  border-b border-t p-4  py-3 bg-clip-text bg-gradient-to-l from-primary-500 via-secondary-500 to-primary-600">Values
         </th>
       </tr>
       </thead>
@@ -23,12 +23,12 @@ function CompositeKeysTable({compositeKeys}: CompositeKeysTableProps) {
             <td className="w-6 text-center border-b">
               {index + 1}
             </td>
-            <td className="border-b">{compositeKey.key.join(', ')}</td>
+            <td className="border-b p-4">{compositeKey.key.join(', ')}</td>
           </tr>
         )) :
         <tr className="text-neutral-900 border-neutral-300">
           <td className="w-6 text-center border-b"></td>
-          <td className=" text-center border-b">
+          <td className=" text-center border-b p-4">
             No Composite Keys Has Been Added Yet
           </td>
         </tr>}
--- formkiq/apps/formkiq-document-console-react/src/app/Components/TagSchemas/tables/tagsTable.tsx
+++ formkiq/apps/formkiq-document-console-react/src/app/Components/TagSchemas/tables/tagsTable.tsx
@@ -9,38 +9,38 @@ type TagsTableProps = {
 function CompositeKeysTable({tags}: TagsTableProps) {
   return (
     <table
-      className="w-full border-collapse text-sm table-fixed border border-neutral-300"
+      className="w-full border-collapse text-sm table-fixed border border-neutral-300 "
     >
       <thead
         className="w-full bg-neutral-100 z-10 pt-2 border-b border-t text-transparent font-bold text-left border-neutral-300">
       <tr>
         <th
-          className=" w-6 border-b border-t p-4 pl-8 py-3 bg-clip-text bg-gradient-to-l from-primary-500 via-secondary-500 to-primary-600"></th>
+          className=" w-6 border-b border-t p-4 py-3 bg-clip-text bg-gradient-to-l from-primary-500 via-secondary-500 to-primary-600"></th>
         <th
-          className=" w-full  border-b border-t p-4 pl-8 py-3 bg-clip-text bg-gradient-to-l from-primary-500 via-secondary-500 to-primary-600">Key
+          className=" w-full  border-b border-t p-4 py-3 bg-clip-text bg-gradient-to-l from-primary-500 via-secondary-500 to-primary-600">Key
         </th>
         <th
-          className=" w-full  border-b border-t p-4 pl-8 py-3 bg-clip-text bg-gradient-to-l from-primary-500 via-secondary-500 to-primary-600">Default Values
+          className=" w-full  border-b border-t p-4 py-3 bg-clip-text bg-gradient-to-l from-primary-500 via-secondary-500 to-primary-600">Default Values
         </th>
         <th
-          className=" w-full  border-b border-t p-4 pl-8 py-3 bg-clip-text bg-gradient-to-l from-primary-500 via-secondary-500 to-primary-600">Allowed Values
+          className=" w-full  border-b border-t p-4 py-3 bg-clip-text bg-gradient-to-l from-primary-500 via-secondary-500 to-primary-600">Allowed Values
         </th>
       </tr>
       </thead>
       <tbody>
       {tags.length > 0 ? tags.map((tag, index) => (
-          <tr key={"compositeKey_" + index} className="text-neutral-900 border-neutral-300">
+          <tr key={"compositeKey_" + index} className="text-neutral-900 border-neutral-300 text-left">
             <td className="w-6 text-center border-b">
               {index + 1}
             </td>
-            <td className="border-b">{tag.key}</td>
-            <td className="border-b">{tag.defaultValues.join(', ')}</td>
-            <td className="border-b">{tag.allowedValues.join(', ')}</td>
+            <td className="border-b p-4">{tag.key}</td>
+            <td className="border-b p-4">{tag.defaultValues.join(', ')}</td>
+            <td className="border-b p-4">{tag.allowedValues.join(', ')}</td>
           </tr>
         )) :
         <tr className="text-neutral-900 border-neutral-300">
           <td className="w-6 text-center border-b"></td>
-          <td className=" text-center border-b">
+          <td className=" text-center border-b p-4">
             No Tags Has Been Added Yet
           </td>
         </tr>}
