9ef190a7d7cdc29ec571879e624187604dbddbe7
Regan Wolfrom <regan.wolfrom@gmail.com>
2024-05-26 13:18:08 -0500

    removed 'TABLES' and 'FORMS' from Tesseract OCR workflow

--- formkiq/apps/formkiq-document-console-react/src/app/Components/Workflows/WorkflowStep/Steps/Ocr.tsx
+++ formkiq/apps/formkiq-document-console-react/src/app/Components/Workflows/WorkflowStep/Steps/Ocr.tsx
@@ -1,14 +1,14 @@
-import {ConfigState} from "../../../../Store/reducers/config";
-import {useMemo} from "react";
-import {useSelector} from "react-redux";
-import {Position} from "reactflow";
-import {Documents, Plus} from "../../../Icons/icons";
-import {DefaultSourceHandle} from "../../Handles/handles";
-import Checkbox from "../NodeComponents/Checkbox";
-import NodeTitle from "../NodeComponents/NodeTitle";
-import NumberInput from "../NodeComponents/NumberInput";
-import ParametersSelector from "../NodeComponents/ParametersSelector";
-import {NodeNameSelector} from "../NodeComponents/NodeNameSelector";
+import { useMemo } from 'react';
+import { useSelector } from 'react-redux';
+import { Position } from 'reactflow';
+import { ConfigState } from '../../../../Store/reducers/config';
+import { Documents, Plus } from '../../../Icons/icons';
+import { DefaultSourceHandle } from '../../Handles/handles';
+import Checkbox from '../NodeComponents/Checkbox';
+import { NodeNameSelector } from '../NodeComponents/NodeNameSelector';
+import NodeTitle from '../NodeComponents/NodeTitle';
+import NumberInput from '../NodeComponents/NumberInput';
+import ParametersSelector from '../NodeComponents/ParametersSelector';
 
 const stepInfo = {
   title: 'Optical Character Recognition (OCR)',
@@ -16,7 +16,7 @@ const stepInfo = {
   numberInputParameters: {
     ocrNumberOfPages: {
       title: 'Number of Pages to Process (from start)',
-      editDescription: '(from start) - use "-1" for no limit',
+      editDescription: '"-1" for no limit',
       defaultValue: -1,
       min: -1,
     },
@@ -29,6 +29,7 @@ const stepInfo = {
         FORMS: 'Form Recognition',
         TABLES: 'Table Recognition',
       },
+      defaultValue: 'TEXT',
     },
     ocrEngine: {
       description: 'OCR Engine to use',
@@ -44,30 +45,38 @@ const stepInfo = {
     },
   },
   decisions: ['APPROVE'],
-}
+};
 
-function Ocr({newStep, setNewStep, isEditing, data, edges, id, addCreatorNode}: any) {
+function Ocr({
+  newStep,
+  setNewStep,
+  isEditing,
+  data,
+  edges,
+  id,
+  addCreatorNode,
+}: any) {
+  const { formkiqVersion } = useSelector(ConfigState);
 
-  const {
-    formkiqVersion,
-  } = useSelector(ConfigState);
-
-  const ocrTypesSelectorOptions = {
+  let ocrTypesSelectorOptions: any = {
     TEXT: 'Text Recognition',
-    FORMS: 'Form Recognition',
-    TABLES: 'Table Recognition',
-  }
+  };
 
   let ocrEngineSelectorOptions: any = {
     TESSERACT: 'Tesseract',
-  }
+  };
 
   // add textract if module enabled
   if (formkiqVersion.modules.includes('ocr')) {
     ocrEngineSelectorOptions = {
       ...ocrEngineSelectorOptions,
       TEXTRACT: 'Textract',
-    }
+    };
+    ocrTypesSelectorOptions = {
+      ...ocrTypesSelectorOptions,
+      FORMS: 'Form Recognition',
+      TABLES: 'Table Recognition',
+    };
   }
 
   const onChange = (value: any, key: any) => {
@@ -80,8 +89,8 @@ function Ocr({newStep, setNewStep, isEditing, data, edges, id, addCreatorNode}: 
     });
   };
   const MAX_CONNECTIONS = 1;
-  let isHandleConnectable = false
-  let connectionsNumber = MAX_CONNECTIONS
+  let isHandleConnectable = false;
+  let connectionsNumber = MAX_CONNECTIONS;
   if (edges) {
     connectionsNumber = edges.filter((e: any) => e.source === id).length;
   }
@@ -89,53 +98,89 @@ function Ocr({newStep, setNewStep, isEditing, data, edges, id, addCreatorNode}: 
     return connectionsNumber < MAX_CONNECTIONS;
   }, [connectionsNumber, MAX_CONNECTIONS]);
 
-
   return (
     <>
-      {isEditing && <NodeNameSelector newStep={newStep} setNewStep={setNewStep} info={stepInfo}/>}
-      {!isEditing && <NodeTitle icon={<Documents/>} title='Optical Character Recognition (OCR)'/>}
+      {isEditing && (
+        <NodeNameSelector
+          newStep={newStep}
+          setNewStep={setNewStep}
+          info={stepInfo}
+        />
+      )}
+      {!isEditing && (
+        <NodeTitle
+          icon={<Documents />}
+          title="Optical Character Recognition (OCR)"
+        />
+      )}
       {!isEditing && <div className="h-px bg-gray-400 my-1.5 w-full"></div>}
-      <ParametersSelector options={ocrTypesSelectorOptions}
-                          description='OCR Parsing strategy to use'
-                          onChange={(value: any) => onChange(value, 'ocrParseTypes')}
-                          selectedValue={isEditing ? (newStep?.parameters?.ocrParseTypes) : data.parameters?.ocrParseTypes}
-                          isEditing={isEditing}/>
+      <ParametersSelector
+        options={ocrTypesSelectorOptions}
+        description="OCR Parsing strategy to use"
+        onChange={(value: any) => onChange(value, 'ocrParseTypes')}
+        selectedValue={
+          isEditing
+            ? newStep?.parameters?.ocrParseTypes
+            : data.parameters?.ocrParseTypes
+        }
+        isEditing={isEditing}
+      />
 
-      <ParametersSelector options={ocrEngineSelectorOptions}
-                          description='OCR Engine to use'
-                          onChange={(value: any) => onChange(value, 'ocrEngine')}
-                          selectedValue={isEditing ? (newStep?.parameters?.ocrEngine) : data.parameters?.ocrEngine}
-                          isEditing={isEditing}/>
+      <ParametersSelector
+        options={ocrEngineSelectorOptions}
+        description="OCR Engine to use"
+        onChange={(value: any) => onChange(value, 'ocrEngine')}
+        selectedValue={
+          isEditing
+            ? newStep?.parameters?.ocrEngine
+            : data.parameters?.ocrEngine
+        }
+        isEditing={isEditing}
+      />
 
       <NumberInput
         description="Number of Pages to Process (from start)"
-        editDescription= '(from start) - use "-1" for no limit'
+        editDescription='use "-1" for no limit'
         defaultValue={-1}
         min={-1}
         onChange={(value: any) => onChange(value, 'ocrNumberOfPages')}
-        selectedValue={isEditing ? (newStep?.parameters?.ocrNumberOfPages) : data.parameters?.ocrNumberOfPages}
-        isEditing={isEditing}/>
+        selectedValue={
+          isEditing
+            ? newStep?.parameters?.ocrNumberOfPages
+            : data.parameters?.ocrNumberOfPages
+        }
+        isEditing={isEditing}
+      />
 
       <Checkbox
         description="PDF Documents convert images to text"
-        onChange={(value: any) => onChange(value, 'addPdfDetectedCharactersAsText')}
-        selectedValue={isEditing ? (newStep?.parameters?.addPdfDetectedCharactersAsText) : data.parameters?.addPdfDetectedCharactersAsText}
-        isEditing={isEditing}/>
+        onChange={(value: any) =>
+          onChange(value, 'addPdfDetectedCharactersAsText')
+        }
+        selectedValue={
+          isEditing
+            ? newStep?.parameters?.addPdfDetectedCharactersAsText
+            : data.parameters?.addPdfDetectedCharactersAsText
+        }
+        isEditing={isEditing}
+      />
 
-      {!isEditing && <DefaultSourceHandle
-        type="source"
-        position={Position.Right}
-        id="approve"
-        maxConnections={1}
-        nodeId={id}
-      ></DefaultSourceHandle>}
+      {!isEditing && (
+        <DefaultSourceHandle
+          type="source"
+          position={Position.Right}
+          id="approve"
+          maxConnections={1}
+          nodeId={id}
+        ></DefaultSourceHandle>
+      )}
       {isHandleConnectable && (
         <div
           className="w-6 mt-6 rounded-full bg-green-400 text-white hover:border-green-700 p-1  cursor-pointer absolute right-[-36px] border-2 border-white hover:text-green-700 nodrag"
-          style={{top: 'calc(50% - 12px)'}}
+          style={{ top: 'calc(50% - 12px)' }}
           onClick={addCreatorNode}
         >
-          <Plus/>
+          <Plus />
         </div>
       )}
     </>
