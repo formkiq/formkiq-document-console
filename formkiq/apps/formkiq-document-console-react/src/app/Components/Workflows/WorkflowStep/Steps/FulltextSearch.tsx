9940b101526c3dfad7881fa68a051035b1932a8c
AnastasiiaPirus <a.pirus2000@gmail.com>
2024-05-29 11:38:12 -0400

    Fixed fulltext serach workflow node title

--- formkiq/apps/formkiq-document-console-react/src/app/Components/Workflows/WorkflowStep/Steps/FulltextSearch.tsx
+++ formkiq/apps/formkiq-document-console-react/src/app/Components/Workflows/WorkflowStep/Steps/FulltextSearch.tsx
@@ -40,9 +40,8 @@ function FulltextSearch({newStep, setNewStep, isEditing, edges, id, addCreatorNo
     <>
       {isEditing && <NodeNameSelector newStep={newStep} setNewStep={setNewStep} info={stepInfo}/>}
       {!isEditing &&
-        <NodeTitle icon={<Search />} title='Send Notification (requires "FROM" address in SES)'/>}
+        <NodeTitle icon={<Search />} title='Fulltext Search'/>}
       {!isEditing && <div className="h-px bg-gray-400 my-1.5 w-full"></div>}
-
 
       {!isEditing && <DefaultSourceHandle
         type="source"
@@ -51,6 +50,7 @@ function FulltextSearch({newStep, setNewStep, isEditing, edges, id, addCreatorNo
         maxConnections={1}
         nodeId={id}
       ></DefaultSourceHandle>}
+      
       {isHandleConnectable && (
         <div
           className="w-6 mt-6 rounded-full bg-green-400 text-white hover:border-green-700 p-1  cursor-pointer absolute right-[-36px] border-2 border-white hover:text-green-700 nodrag"
