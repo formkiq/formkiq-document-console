8140d9d88237d4693b20313f345df34c94bf1a5c
Regan Wolfrom <regan.wolfrom@gmail.com>
2024-06-23 14:02:47 -0500

    tweaks to attribute changes

--- formkiq/apps/formkiq-document-console-react/src/app/Components/Generic/Listboxes/RadioListbox.tsx
+++ formkiq/apps/formkiq-document-console-react/src/app/Components/Generic/Listboxes/RadioListbox.tsx
@@ -1,58 +1,88 @@
-import {Listbox} from "@headlessui/react";
-import {CheckedRadio, ChevronDown, UncheckedRadio} from "../../Icons/icons";
-
+import { Listbox } from '@headlessui/react';
+import { CheckedRadio, ChevronDown, UncheckedRadio } from '../../Icons/icons';
 
 type RadioListboxPropsType = {
-  values: string[]
-  titles: string[]
-  selectedValue: string
-  setSelectedValue: (value: any) => void
-  icon?: () => JSX.Element
-  iconsColor?: string[]
-  placeholderText?: string
+  values: string[];
+  titles: string[];
+  selectedValue: string;
+  setSelectedValue: (value: any) => void;
+  icon?: () => JSX.Element;
+  iconsColor?: string[];
+  placeholderText?: string;
 };
 
 function RadioListbox({
-                        values,
-                        titles,
-                        selectedValue,
-                        setSelectedValue,
-                        icon,
-                        iconsColor,
-                        placeholderText
-                      }: RadioListboxPropsType) {
+  values,
+  titles,
+  selectedValue,
+  setSelectedValue,
+  icon,
+  iconsColor,
+  placeholderText,
+}: RadioListboxPropsType) {
   return (
-    <div className='relative h-full'>
+    <div className="relative h-full">
       <Listbox value={selectedValue} onChange={setSelectedValue}>
-        <Listbox.Button
-          className="h-full max-h-8 bg-neutral-100 px-4 w-full text-start font-medium flex flex-row justify-between items-center text-xs  rounded-md">
+        <Listbox.Button className="h-full max-h-8 bg-neutral-100 px-4 w-full text-start font-medium flex flex-row justify-between items-center text-xs  rounded-md">
           <div className="flex items-center justify-start gap-2 truncate">
-            {icon && <div className="w-3 h-3 text-neutral-300"
-                          style={{
-                            minWidth: '12px',
-                            color: iconsColor ? iconsColor[values.indexOf(selectedValue)] : '#cbd5e1'
-                          }}>{icon()}</div>}
-            <span
-              className="block truncate">{(values.indexOf(selectedValue) === -1 && placeholderText) ? placeholderText : titles[values.indexOf(selectedValue)]}</span>
+            {icon && (
+              <div
+                className="w-3 h-3 text-neutral-300"
+                style={{
+                  minWidth: '12px',
+                  color: iconsColor
+                    ? iconsColor[values.indexOf(selectedValue)]
+                    : '#cbd5e1',
+                }}
+              >
+                {icon()}
+              </div>
+            )}
+            <span className="block truncate">
+              {values.indexOf(selectedValue) === -1 && placeholderText
+                ? placeholderText
+                : titles[values.indexOf(selectedValue)]}
+            </span>
           </div>
-          <div className="w-3 text-neutral-500" style={{minWidth: '12px'}}>{<ChevronDown/>}</div>
+          <div
+            className="ml-2 w-3 text-neutral-500"
+            style={{ minWidth: '12px' }}
+          >
+            {<ChevronDown />}
+          </div>
         </Listbox.Button>
-        <Listbox.Options
-          className="absolute top-9 right-0 h-48 overflow-y-scroll bg-white shadow-md border border-neutral-100 font-medium z-50">
+        <Listbox.Options className="absolute top-9 right-0 h-48 overflow-y-scroll bg-white shadow-md border border-neutral-100 font-medium z-50">
           {values.map((key, index) => (
-            <Listbox.Option key={key} value={key} className="h-12 hover:bg-neutral-200 px-6 flex items-center text-xs">
+            <Listbox.Option
+              key={key}
+              value={key}
+              className="h-12 hover:bg-neutral-200 px-6 flex items-center text-xs"
+            >
               <div className="relative w-full h-full flex items-center">
-                <input type="radio" name="status" value={key} checked={selectedValue === key} readOnly
-                       className="absolute left-0 top-0 h-full w-full cursor-pointer opacity-0"/>
-                <label className="flex items-center justify-between gap-2 w-full"><span
-                  className="block truncate">{titles[index]}</span>
-                  <div className="w-4">{selectedValue === key ? <CheckedRadio/> : <UncheckedRadio/>}</div>
+                <input
+                  type="radio"
+                  name="status"
+                  value={key}
+                  checked={selectedValue === key}
+                  readOnly
+                  className="absolute left-0 top-0 h-full w-full cursor-pointer opacity-0"
+                />
+                <label className="flex items-center justify-between gap-2 w-full">
+                  <span className="block truncate">{titles[index]}</span>
+                  <div className="w-4">
+                    {selectedValue === key ? (
+                      <CheckedRadio />
+                    ) : (
+                      <UncheckedRadio />
+                    )}
+                  </div>
                 </label>
               </div>
             </Listbox.Option>
           ))}
         </Listbox.Options>
-      </Listbox></div>
+      </Listbox>
+    </div>
   );
 }
 
--- formkiq/apps/formkiq-document-console-react/src/app/Views/Account/managePolicy.tsx
+++ formkiq/apps/formkiq-document-console-react/src/app/Views/Account/managePolicy.tsx
@@ -1,21 +1,23 @@
-import {useEffect, useState} from 'react';
-import {Helmet} from 'react-helmet-async';
-import {Pencil, Trash} from '../../Components/Icons/icons';
-import RegoEditor from '../../Components/TextEditors/RegoEditor';
-import {DocumentsService} from '../../helpers/services/documentsService';
-import ButtonPrimaryGradient from "../../Components/Generic/Buttons/ButtonPrimaryGradient";
-import {useParams} from "react-router-dom";
+import { useEffect, useState } from 'react';
+import { Helmet } from 'react-helmet-async';
+import { useParams } from 'react-router-dom';
 import CreatePolicyModal from '../../Components/AccessControl/CreatePolicyModal';
-import EditPolicyModal from "../../Components/AccessControl/EditPolicyModal";
-import {useAppDispatch} from "../../Store/store";
-import {openDialog as openNotificationDialog} from "../../Store/reducers/globalNotificationControls";
-import {openDialog as openConfirmationDialog} from "../../Store/reducers/globalConfirmControls";
+import EditPolicyModal from '../../Components/AccessControl/EditPolicyModal';
+import ButtonPrimaryGradient from '../../Components/Generic/Buttons/ButtonPrimaryGradient';
+import { Pencil, Trash } from '../../Components/Icons/icons';
+import RegoEditor from '../../Components/TextEditors/RegoEditor';
+import { openDialog as openConfirmationDialog } from '../../Store/reducers/globalConfirmControls';
+import { openDialog as openNotificationDialog } from '../../Store/reducers/globalNotificationControls';
+import { useAppDispatch } from '../../Store/store';
+import { DocumentsService } from '../../helpers/services/documentsService';
 
 export function AccessControl() {
-  const {siteId} = useParams()
+  const { siteId } = useParams();
   const [allPolicyItems, setAllPolicyItems] = useState<any[]>([]);
-  const [isCreatePolicyModalOpen, setIsCreatePolicyModalOpen] = useState<boolean>(false);
-  const [isEditPolicyModalOpen, setIsEditPolicyModalOpen] = useState<boolean>(false);
+  const [isCreatePolicyModalOpen, setIsCreatePolicyModalOpen] =
+    useState<boolean>(false);
+  const [isEditPolicyModalOpen, setIsEditPolicyModalOpen] =
+    useState<boolean>(false);
   const [editPolicyIndex, setEditPolicyIndex] = useState<number>(-1);
   const dispatch = useAppDispatch();
 
@@ -31,17 +33,16 @@ export function AccessControl() {
   }
 
   useEffect(() => {
-    updatePolicyItems()
+    updatePolicyItems();
   }, []);
 
-
   function onCreatePolicyModalClose() {
-    updatePolicyItems()
+    updatePolicyItems();
     setIsCreatePolicyModalOpen(false);
   }
 
   function onEditPolicyModalClose() {
-    updatePolicyItems()
+    updatePolicyItems();
     setIsEditPolicyModalOpen(false);
   }
 
@@ -52,38 +53,51 @@ export function AccessControl() {
       newPolicyItems.forEach((item: any, i: number) => {
         newPolicyItems[i] = {
           ...item,
-          type: "ALLOW"
-        }
-      })
+          type: 'ALLOW',
+        };
+      });
       newPolicyItems.splice(index, 1);
       if (newPolicyItems.length > 0) {
-        DocumentsService.setOpenPolicyAgentPolicyItems(siteId, {policyItems: newPolicyItems}).then((response) => {
+        DocumentsService.setOpenPolicyAgentPolicyItems(siteId, {
+          policyItems: newPolicyItems,
+        }).then((response) => {
           if (response.status === 200) {
-            updatePolicyItems()
+            updatePolicyItems();
           } else {
-            dispatch(openNotificationDialog({
-              dialogTitle: `Error.\n ${response.errors.map((error: any) => error.error).join(", \n")}`
-            }))
+            dispatch(
+              openNotificationDialog({
+                dialogTitle: `Error.\n ${response.errors
+                  .map((error: any) => error.error)
+                  .join(', \n')}`,
+              })
+            );
           }
-        })
+        });
       } else {
-        DocumentsService.deleteOpenPolicyAgentPolicyItems(siteId).then((response) => {
-          if (response.status === 200) {
-            updatePolicyItems()
-          } else {
-            dispatch(openNotificationDialog({
-              dialogTitle: `Error.\n ${response.errors.map((error: any) => error.error).join(", \n")}`
-            }))
+        DocumentsService.deleteOpenPolicyAgentPolicyItems(siteId).then(
+          (response) => {
+            if (response.status === 200) {
+              updatePolicyItems();
+            } else {
+              dispatch(
+                openNotificationDialog({
+                  dialogTitle: `Error.\n ${response.errors
+                    .map((error: any) => error.error)
+                    .join(', \n')}`,
+                })
+              );
+            }
           }
-        })
+        );
       }
     }
 
-    dispatch(openConfirmationDialog({
-      dialogTitle: "Are you sure you want to delete this policy item?",
-      callback: deletePolicyItem
-    }))
-
+    dispatch(
+      openConfirmationDialog({
+        dialogTitle: 'Are you sure you want to delete this policy item?',
+        callback: deletePolicyItem,
+      })
+    );
   }
 
   return (
@@ -97,48 +111,68 @@ export function AccessControl() {
             Edit Policy Items
           </h6>
           <div className="w-[2px] min-w-[2px] block h-6 bg-neutral-500 "></div>
-          <a className="text-sm font-bold text-gray-500 hover:text-primary-600 cursor-pointer whitespace-nowrap"
-             href={`/account/accessControl?selectedPolicySiteId=${siteId}`}>
+          <a
+            className="text-sm font-bold text-gray-500 hover:text-primary-600 cursor-pointer whitespace-nowrap"
+            href={`/account/accessControl?selectedPolicySiteId=${siteId}`}
+          >
             Back To Policy View
           </a>
         </div>
-        <ButtonPrimaryGradient className="h-8" onClick={() => setIsCreatePolicyModalOpen(true)}>+ Add
-          New Policy Item</ButtonPrimaryGradient>
+        <ButtonPrimaryGradient
+          className="h-8"
+          onClick={() => setIsCreatePolicyModalOpen(true)}
+        >
+          + Add New Policy Item
+        </ButtonPrimaryGradient>
       </div>
       <h6 className="my-2 pl-2 text-base tracking-normal leading-10 font-medium text-neutral-900 sm:leading-none">
         Site ID: {siteId}
       </h6>
-      {allPolicyItems && allPolicyItems.map((policyItem: any, index: number) => (
-        <div key={"policyItem_" + index}>
-          <div className="flex justify-between items-center px-2 my-2 h-8 gap-2">
-            <h6 className="font-bold text-neutral-900">Policy: {index + 1}</h6>
-            <div className="flex items-center gap-2">
-              <button className="h-6 w-6 hover:text-primary-500" onClick={() => {
-                setEditPolicyIndex(index)
-                setIsEditPolicyModalOpen(true)
-              }}>
-                <Pencil/>
-              </button>
-              <button className="h-5 w-5 hover:text-primary-500" onClick={() => onDeletePolicyItem(index)}>
-                <Trash/>
-              </button>
+      {allPolicyItems &&
+        allPolicyItems.map((policyItem: any, index: number) => (
+          <div key={'policyItem_' + index}>
+            <div className="flex justify-between items-center px-2 my-2 h-8 gap-2">
+              <h6 className="font-bold text-neutral-900">
+                Policy Item: {index + 1}
+              </h6>
+              <div className="flex items-center gap-2">
+                <button
+                  className="h-6 w-6 hover:text-primary-500"
+                  onClick={() => {
+                    setEditPolicyIndex(index);
+                    setIsEditPolicyModalOpen(true);
+                  }}
+                >
+                  <Pencil />
+                </button>
+                <button
+                  className="h-5 w-5 hover:text-primary-500"
+                  onClick={() => onDeletePolicyItem(index)}
+                >
+                  <Trash />
+                </button>
+              </div>
             </div>
+            <RegoEditor
+              content={policyItem.policy}
+              readOnly="nocursor"
+              onChange={() => {}}
+            />
           </div>
-          <RegoEditor
-            content={policyItem.policy}
-            readOnly="nocursor"
-            onChange={() => {
-            }}/>
-        </div>
-      ))}
-      <CreatePolicyModal isOpened={isCreatePolicyModalOpen}
-                         onClose={onCreatePolicyModalClose}
-                         policyItems={allPolicyItems}
-                         siteId={siteId ? siteId : 'default'}/>
-      <EditPolicyModal isOpened={isEditPolicyModalOpen}
-                       onClose={onEditPolicyModalClose}
-                       policyItems={allPolicyItems}
-                       siteId={siteId ? siteId : 'default'} index={editPolicyIndex}/>
+        ))}
+      <CreatePolicyModal
+        isOpened={isCreatePolicyModalOpen}
+        onClose={onCreatePolicyModalClose}
+        policyItems={allPolicyItems}
+        siteId={siteId ? siteId : 'default'}
+      />
+      <EditPolicyModal
+        isOpened={isEditPolicyModalOpen}
+        onClose={onEditPolicyModalClose}
+        policyItems={allPolicyItems}
+        siteId={siteId ? siteId : 'default'}
+        index={editPolicyIndex}
+      />
     </>
   );
 }
