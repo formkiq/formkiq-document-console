c82dbad714e561ee69205631ac7e807600c539da
AnastasiiaPirus <a.pirus2000@gmail.com>
2024-07-12 15:20:31 -0400

    Added site dropdown on API Keys page
    - Added site dropdown to navbar on API Keys page.
    - Updated API Keys page to display only one site keys.
    - Updated navbar changeSiteId method to apply to paths with subfolders.

--- formkiq/apps/formkiq-document-console-react/src/app/Views/Integrations/Api/apiKeys.tsx
+++ formkiq/apps/formkiq-document-console-react/src/app/Views/Integrations/Api/apiKeys.tsx
@@ -1,26 +1,21 @@
-import { useEffect, useState } from 'react';
-import { Helmet } from 'react-helmet-async';
-import { useSelector } from 'react-redux';
-import { ArrowBottom, ArrowRight } from '../../../Components/Icons/icons';
+import {useEffect, useState} from 'react';
+import {Helmet} from 'react-helmet-async';
+import {useSelector} from 'react-redux';
 import ApiKeyList from '../../../Components/Integrations/ApiKeyList/ApiKeyList';
 import NewApiKeyModal from '../../../Components/Integrations/NewApiKey/newApiKey';
-import { AuthState } from '../../../Store/reducers/auth';
-import { openDialog } from '../../../Store/reducers/globalConfirmControls';
-import { useAppDispatch } from '../../../Store/store';
-import { DocumentsService } from '../../../helpers/services/documentsService';
-
-type KeyItem = {
-  siteId: string;
-  readonly: boolean;
-  keys: [] | null;
-};
+import {AuthState} from '../../../Store/reducers/auth';
+import {openDialog} from '../../../Store/reducers/globalConfirmControls';
+import {useAppDispatch} from '../../../Store/store';
+import {DocumentsService} from '../../../helpers/services/documentsService';
+import {getCurrentSiteInfo, getUserSites} from "../../../helpers/services/toolService";
+import {useLocation, useNavigate} from "react-router-dom";
 
 export function ApiKeys() {
   const dispatch = useAppDispatch();
   let userSite: any = null;
   let defaultSite: any = null;
   const workspaceSites: any[] = [];
-  const { user } = useSelector(AuthState);
+  const {user} = useSelector(AuthState);
   if (user && user.sites) {
     user.sites.forEach((site: any) => {
       if (site.siteId === user.email) {
@@ -32,33 +27,47 @@ export function ApiKeys() {
       }
     });
   }
-  const currentSite = userSite
-    ? userSite
-    : defaultSite
-    ? defaultSite
-    : workspaceSites[0];
-  if (currentSite === null) {
-    alert('No site configured for this user');
-    window.location.href = '/';
-  }
 
-  const [userSiteExpanded, setUserSiteExpanded] = useState(true);
-  const [userSiteApiKeys, setUserSiteApiKeys] = useState(null);
-  const [defaultSiteExpanded, setDefaultSiteExpanded] = useState(true);
-  const [defaultSiteApiKeys, setDefaultSiteApiKeys] = useState(null);
-  const [workspaceKeys, setWorkspaceKeys] = useState<KeyItem[]>([]);
-  const [workspacesExpanded, setWorkspacesExpanded] = useState(false);
+  const {hasUserSite, hasDefaultSite, hasWorkspaces} = getUserSites(user);
+
+  const pathname = decodeURI(useLocation().pathname);
+  const {
+    siteId,
+  } = getCurrentSiteInfo(
+    pathname,
+    user,
+    hasUserSite,
+    hasDefaultSite,
+    hasWorkspaces,
+    workspaceSites
+  );
+
+  const navigate = useNavigate();
+  const [currentSiteId, setCurrentSiteId] = useState(siteId);
+  const [currentSite, setCurrentSite] = useState<any>(null)
+
+  useEffect(() => {
+    if (currentSiteId) {
+      let site: any;
+      if (user?.email && currentSiteId === user.email) {
+        site = userSite
+      } else if (currentSiteId === 'default') {
+        site = defaultSite
+      } else {
+        site = workspaceSites.find(site => site.siteId === currentSiteId)
+      }
+      setCurrentSite(site);
+      if (!site) {
+        alert('No site configured for this user');
+        window.location.href = '/';
+      }
+    }
+  }, [currentSiteId])
+
+  const [currentSiteApiKeys, setCurrentSiteApiKeys] = useState<any>(null);
   const [isNewModalOpened, setNewModalOpened] = useState(false);
   const [newModalSiteId, setNewModalSiteId] = useState('default');
-  const toggleUserSiteExpand = () => {
-    setUserSiteExpanded(!userSiteExpanded);
-  };
-  const toggleDefaultSiteExpand = () => {
-    setDefaultSiteExpanded(!defaultSiteExpanded);
-  };
-  const toggleWorkspacesExpand = () => {
-    setWorkspacesExpanded(!workspacesExpanded);
-  };
+
   const onNewClick = (event: any, siteId: string) => {
     setNewModalSiteId(siteId);
     setNewModalOpened(true);
@@ -73,73 +82,52 @@ export function ApiKeys() {
   }, [isNewModalOpened]);
 
   useEffect(() => {
+    const recheckSiteInfo = getCurrentSiteInfo(
+      pathname,
+      user,
+      hasUserSite,
+      hasDefaultSite,
+      hasWorkspaces,
+      workspaceSites
+    );
+    if (recheckSiteInfo.siteRedirectUrl.length) {
+      navigate(
+        {
+          pathname: `${recheckSiteInfo.siteRedirectUrl}`,
+        },
+        {
+          replace: true,
+        }
+      );
+    }
+    setCurrentSiteId(recheckSiteInfo.siteId);
+  }, [pathname]);
+
+  useEffect(() => {
     updateApiKeys();
-  }, [user]);
+  }, [user, currentSiteId]);
 
   const setApiKeys = (apiKeys: [], siteId: string, readonly: boolean) => {
-    if (siteId === user?.email) {
-      // NOTE: does not allow for a readonly user site
-      setUserSiteApiKeys(apiKeys as any);
-    } else if (siteId === 'default') {
-      // NOTE: does not allow for a readonly default site
-      setDefaultSiteApiKeys(apiKeys as any);
-    }
+    setCurrentSiteApiKeys(apiKeys as any);
   };
 
-  const updateApiKeys = () => {
-    if (userSite) {
-      let readonly = false;
-      if (userSite.permission && userSite.permission === 'READ_ONLY') {
-        readonly = true;
-      }
-      DocumentsService.getApiKeys(userSite.siteId).then(
-        (apiKeysResponse: any) => {
-          setApiKeys(apiKeysResponse.apiKeys, userSite.siteId, readonly);
-        }
-      );
-    }
-    if (defaultSite) {
-      let readonly = false;
-      if (defaultSite.permission && defaultSite.permission === 'READ_ONLY') {
-        readonly = true;
-      }
-      DocumentsService.getApiKeys(defaultSite.siteId).then(
-        (apiKeysResponse: any) => {
-          setApiKeys(apiKeysResponse.apiKeys, defaultSite.siteId, readonly);
-        }
-      );
-    }
-    if (workspaceSites.length > 0) {
-      const initialWorkspaceApiKeysPromises = workspaceSites.map((item) => {
-        let readonly = false;
-        if (item.permission && item.permission === 'READ_ONLY') {
-          readonly = true;
-        }
-        return DocumentsService.getApiKeys(item.siteId).then(
-          (apiKeysResponse: any) => {
-            return {
-              keys: apiKeysResponse.apiKeys,
-              siteId: item.siteId,
-              readonly,
-            };
-          }
-        );
-      });
 
-      Promise.all(initialWorkspaceApiKeysPromises)
-        .then((initialWorkspaceApiKeys) => {
-          setWorkspaceKeys(initialWorkspaceApiKeys);
-        })
-        .catch((error) => {
-          // Handle any errors that occurred during the requests
-          console.error('Error fetching webhooks:', error);
-        });
+  const updateApiKeys = () => {
+
+    let readonly = false;
+    if (currentSite && currentSite.permission && currentSite.permission === 'READ_ONLY') {
+      readonly = true;
     }
+    DocumentsService.getApiKeys(currentSiteId).then(
+      (apiKeysResponse: any) => {
+        setApiKeys(apiKeysResponse.apiKeys, currentSiteId, readonly);
+      }
+    );
   };
 
   const deleteApiKey = (apiKey: string, siteId: string) => {
     const deleteFunc = async () => {
-      setUserSiteApiKeys(null);
+      setCurrentSiteApiKeys(null);
       await DocumentsService.deleteApiKey(apiKey, siteId).then(
         (apiKeysResponse: any) => {
           updateApiKeys();
@@ -159,111 +147,29 @@ export function ApiKeys() {
       <Helmet>
         <title>Api Keys</title>
       </Helmet>
-      <div className="p-4 max-w-screen-lg font-semibold mb-4">
+      <div className="p-4 max-w-screen-lg font-semibold">
         FormKiQ allows for API authentication using API Keys
       </div>
       <div className="p-4">
-        {userSite && (
+        {currentSite && (
           <>
             <div
               className="w-full flex self-start text-gray-600 hover:text-gray-500 justify-center lg:justify-start whitespace-nowrap px-2 py-2 cursor-pointer"
-              onClick={toggleUserSiteExpand}
             >
-              <div className="flex justify-end mt-3 mr-1">
-                {userSiteExpanded ? <ArrowBottom /> : <ArrowRight />}
-              </div>
               <div className="pl-1 uppercase text-base">
-                Api Keys: My Documents
+                Api Keys
                 <span className="block normal-case">
-                  {' '}
-                  (Site ID: {userSite.siteId})
+                  (Site ID: {currentSiteId})
                 </span>
               </div>
             </div>
-            {userSiteExpanded && (
-              <ApiKeyList
-                siteId={userSite.siteId}
-                apiKeys={userSiteApiKeys}
-                isSiteReadOnly={userSite.readonly}
-                onDelete={deleteApiKey}
-                onNewClick={onNewClick}
-              ></ApiKeyList>
-            )}
-          </>
-        )}
-        {defaultSite && defaultSite.siteId && (
-          <>
-            <div
-              className="w-full flex self-start text-neutral-900 hover:text-neutral-600 justify-center lg:justify-start whitespace-nowrap px-2 py-2 cursor-pointer"
-              onClick={toggleDefaultSiteExpand}
-            >
-              <div className="flex justify-end mt-3 mr-1">
-                {defaultSiteExpanded ? <ArrowBottom /> : <ArrowRight />}
-              </div>
-              <div className="pl-1 uppercase text-base">
-                {userSite ? (
-                  <span>
-                    API Keys: Team Documents
-                    <span className="block normal-case">
-                      (Site ID: default)
-                    </span>
-                  </span>
-                ) : (
-                  <span>
-                    API Keys: Documents
-                    <span className="block normal-case">
-                      (Site ID: default)
-                    </span>
-                  </span>
-                )}
-              </div>
-            </div>
-            {defaultSiteExpanded && (
-              <ApiKeyList
-                apiKeys={defaultSiteApiKeys}
-                onDelete={deleteApiKey}
-                siteId={defaultSite.siteId}
-                isSiteReadOnly={defaultSite.readonly}
-                onNewClick={onNewClick}
-              ></ApiKeyList>
-            )}
-          </>
-        )}
-        {workspaceSites.length > 0 && (
-          <>
-            <div
-              className="w-full flex self-start text-neutral-900 hover:text-neutral-600 justify-center lg:justify-start whitespace-nowrap px-2 py-2 cursor-pointer"
-              onClick={toggleWorkspacesExpand}
-            >
-              <div className="flex justify-end mt-3 mr-1">
-                {workspacesExpanded ? <ArrowBottom /> : <ArrowRight />}
-              </div>
-              <div className="pl-1 uppercase text-base">
-                API Keys: Workspaces
-              </div>
-            </div>
-            {workspacesExpanded &&
-              workspaceKeys.map((item: KeyItem, i: number) => {
-                return (
-                  <div key={i}>
-                    <div className="mt-4 ml-4 flex flex-wrap w-full">
-                      <div className="pl-1 uppercase text-sm">
-                        API Keys:{' '}
-                        <span className="normal-case">{item.siteId}</span>
-                      </div>
-                    </div>
-                    <div className="mt-4 ml-4">
-                      <ApiKeyList
-                        apiKeys={item.keys}
-                        siteId={item.siteId}
-                        isSiteReadOnly={item.readonly}
-                        onDelete={deleteApiKey}
-                        onNewClick={onNewClick}
-                      ></ApiKeyList>
-                    </div>
-                  </div>
-                );
-              })}
+            <ApiKeyList
+              siteId={currentSiteId}
+              apiKeys={currentSiteApiKeys}
+              isSiteReadOnly={currentSite.readonly}
+              onDelete={deleteApiKey}
+              onNewClick={onNewClick}
+            ></ApiKeyList>
           </>
         )}
       </div>
--- formkiq/apps/formkiq-document-console-react/src/app/helpers/services/toolService.ts
+++ formkiq/apps/formkiq-document-console-react/src/app/helpers/services/toolService.ts
@@ -305,6 +305,25 @@ export function getCurrentSiteInfo(
           )}`;
         }
       }
+    } else if (pathname.indexOf('/integrations/apiKeys') === 0) {
+      if (pathname.indexOf('/integrations/apiKeys/workspaces') === 0) {
+        currentSiteInfo.siteId = pathname.substring(33).split('/')[0]; // 33 is the length of '/integrations/apiKeys/workspaces'
+        currentSiteInfo.siteDocumentsRootName = `API Keys: ${(
+          currentSiteInfo.siteId as any
+        ).replaceAll('_', ' ')}`;
+      } else {
+        if (hasDefaultSite) {
+          currentSiteInfo.siteId = 'default';
+          currentSiteInfo.siteDocumentsRootName = 'API Keys';
+        } else if (hasWorkspaces) {
+          currentSiteInfo.siteId = workspaceSites[0].siteId;
+          currentSiteInfo.siteRedirectUrl = `/integrations/apiKeys/workspaces/${workspaceSites[0].siteId}`;
+          currentSiteInfo.siteDocumentsRootName = `Workspace: ${workspaceSites[0].siteId.replaceAll(
+            '_',
+            ' '
+          )}`;
+        }
+      }
     } else {
       if (hasDefaultSite) {
         currentSiteInfo.siteId = 'default';
