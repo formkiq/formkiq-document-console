8641d14175fc71d584f1d3bc1d5ebbdce9626c3a
Regan Wolfrom <regan.wolfrom@gmail.com>
2024-07-15 14:05:54 -0500

    added select on focus for number input

--- formkiq/apps/formkiq-document-console-react/src/app/Components/Workflows/WorkflowStep/NodeComponents/NumberInput.tsx
+++ formkiq/apps/formkiq-document-console-react/src/app/Components/Workflows/WorkflowStep/NodeComponents/NumberInput.tsx
@@ -1,26 +1,30 @@
-import {ChangeEvent, useEffect} from "react";
-import DisplayValue from "./DisplayValue";
+import { ChangeEvent, useEffect } from 'react';
+import DisplayValue from './DisplayValue';
 
 const NumberInput = ({
-                       defaultValue,
-                       description,
-                       editDescription,
-                       onChange,
-                       selectedValue,
-                       isEditing,
-                       min,
-                       max
-                     }: any
-) => {
-
+  defaultValue,
+  description,
+  editDescription,
+  onChange,
+  selectedValue,
+  isEditing,
+  min,
+  max,
+}: any) => {
   const handleNumberInput = (name: string) => {
-    if(!onChange) return;
-    onChange(name)
+    if (!onChange) return;
+    onChange(name);
+  };
+
+  const handleNumberFocus = (target: EventTarget | null) => {
+    if (target instanceof HTMLInputElement) {
+      target.select();
+    }
   };
 
   const parameterValue = () => {
     if (selectedValue) {
-      return selectedValue
+      return selectedValue;
     } else if (defaultValue) {
       return defaultValue;
     } else {
@@ -30,33 +34,40 @@ const NumberInput = ({
 
   // if default value set, update the step
   useEffect(() => {
-      if (defaultValue&&isEditing) {
-          handleNumberInput(defaultValue);
-      }
+    if (defaultValue && isEditing) {
+      handleNumberInput(defaultValue);
+    }
   }, [defaultValue]);
 
   return (
-    <>{isEditing ? <>
-        <div className="text-sm text-neutral-900 mt-4">
-          {description}:
-        </div>
+    <>
+      {isEditing ? (
+        <>
+          <div className="text-sm text-neutral-900 mt-4">{description}:</div>
 
-        {editDescription &&
-          <div className="text-xs text-neutral-700 mb-2">
-            {editDescription}</div>}
+          {editDescription && (
+            <div className="text-xs text-neutral-700 mb-2">
+              {editDescription}
+            </div>
+          )}
 
-        <input
-          type="number"
-          className="border border-neutral-300 rounded-md p-2 w-full nodrag nowheel"
-          value={parameterValue()}
-          onInput={(event: ChangeEvent<HTMLInputElement>) =>
-            handleNumberInput(event.target.value)
-          }
-          min={min}
-          max={max}
-        />
-      </> :
-      <DisplayValue description={description} value={selectedValue}/>}
+          <input
+            type="number"
+            className="border border-neutral-300 rounded-md p-2 w-full nodrag nowheel"
+            value={parameterValue()}
+            onFocus={(event: React.FocusEvent<HTMLInputElement>) =>
+              handleNumberFocus(event.target)
+            }
+            onInput={(event: ChangeEvent<HTMLInputElement>) =>
+              handleNumberInput(event.target.value)
+            }
+            min={min}
+            max={max}
+          />
+        </>
+      ) : (
+        <DisplayValue description={description} value={selectedValue} />
+      )}
     </>
   );
 };
