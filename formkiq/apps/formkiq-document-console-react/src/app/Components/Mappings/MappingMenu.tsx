1a086f0f846f6938f70872345ed61e34ca0009ef
AnastasiiaPirus <a.pirus2000@gmail.com>
2024-08-28 14:08:33 -0400

    Added return to mappings link to mapping page

--- formkiq/apps/formkiq-document-console-react/src/app/Components/Mappings/MappingMenu.tsx
+++ formkiq/apps/formkiq-document-console-react/src/app/Components/Mappings/MappingMenu.tsx
@@ -3,6 +3,7 @@ import { Mapping } from '../../helpers/types/mappings';
 import { useState } from 'react';
 import ButtonTertiary from '../Generic/Buttons/ButtonTertiary';
 import ButtonPrimaryGradient from '../Generic/Buttons/ButtonPrimaryGradient';
+import { Link, useLocation } from 'react-router-dom';
 
 type GroupMenuPropsType = {
   onMappingDelete: () => void;
@@ -15,8 +16,8 @@ function MappingMenu({
   mapping,
   saveChanges,
 }: GroupMenuPropsType) {
+  const pathname = decodeURI(useLocation().pathname);
   const [isEditing, setIsEditing] = useState(false);
-  const [isEditingDescription, setIsEditingDescription] = useState(false);
   const [name, setName] = useState(mapping.name);
   const [description, setDescription] = useState(mapping.description);
   const onSave = () => {
@@ -55,13 +56,21 @@ function MappingMenu({
               <div className="h-6 w-px bg-neutral-300"></div>
             </>
           ) : (
-            <button
-              title="Edit"
-              className="w-5 h-5 hover:text-primary-500"
-              onClick={() => setIsEditing(!isEditing)}
-            >
-              <Pencil />
-            </button>
+            <>
+              <Link to={pathname.split('/').slice(0, -1).join('/')}
+                    className="text-neutral-500 hover:text-primary-500"
+              >
+                Return To Mappings
+              </Link>
+              <div className="h-6 w-px bg-neutral-300"></div>
+              <button
+                title="Edit"
+                className="w-5 h-5 hover:text-primary-500"
+                onClick={() => setIsEditing(!isEditing)}
+              >
+                <Pencil />
+              </button>
+            </>
           )}
           <button
             title="Delete"
@@ -84,7 +93,7 @@ function MappingMenu({
             onChange={(e) => setDescription(e.target.value)}
           />
         ) : (
-            <p className="text-sm">{mapping.description}</p>
+          <p className="text-sm">{mapping.description}</p>
         )}
       </div>
     </>
