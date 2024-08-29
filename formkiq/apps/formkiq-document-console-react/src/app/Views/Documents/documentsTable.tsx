9e3821c9d2be0d81aa576762536f151e72be3c67
AnastasiiaPirus <a.pirus2000@gmail.com>
2024-08-29 13:25:16 -0400

    Run Prettier in documentsTable

--- formkiq/apps/formkiq-document-console-react/src/app/Views/Documents/documentsTable.tsx
+++ formkiq/apps/formkiq-document-console-react/src/app/Views/Documents/documentsTable.tsx
@@ -13,7 +13,7 @@ import { ILine } from '../../helpers/types/line';
 import { useQueueId } from '../../hooks/queue-id.hook';
 import { useSubfolderUri } from '../../hooks/subfolder-uri.hook';
 import { EmptyDocumentsTable } from './EmptyDocumentsTable';
-import {useLocation, useNavigate} from "react-router-dom";
+import { useLocation, useNavigate } from 'react-router-dom';
 
 type DocumentTableProps = {
   documentsWrapperRef: Ref<any>;
@@ -43,7 +43,7 @@ type DocumentTableProps = {
   deleteFromPendingArchive: (file: IDocument) => void;
   archiveStatus: string;
   trackScrolling: () => void;
-  infoDocumentId:  string;
+  infoDocumentId: string;
   onDocumentInfoClick: () => void;
 };
 
@@ -76,33 +76,38 @@ export const DocumentsTable = (props: DocumentTableProps) => {
   } = props;
 
   const { formkiqVersion, useIndividualSharing } = useSelector(ConfigState);
-  const { documents, folders, loadingStatus, isLastSearchPageLoaded } = useSelector(DocumentListState);
+  const { documents, folders, loadingStatus, isLastSearchPageLoaded } =
+    useSelector(DocumentListState);
 
   const subfolderUri = useSubfolderUri();
   const queueId = useQueueId();
   const navigate = useNavigate();
   const pathname = decodeURI(useLocation().pathname);
   const search = useLocation().search;
-  const scrollToDocumentLine = new URLSearchParams(search).get('scrollToDocumentLine');
+  const scrollToDocumentLine = new URLSearchParams(search).get(
+    'scrollToDocumentLine'
+  );
 
   useEffect(() => {
     // load more items if bottom of a table is visible
-    trackScrolling()
     if (!documents || documents.length === 0) return; // prevent execution if documents are not loaded yet
+    trackScrolling();
     // when user opens document folder after viewing document, scroll to list to display document line
     if (!scrollToDocumentLine) return;
     if (loadingStatus !== RequestStatus.fulfilled) return;
-    const documentIndex = documents.findIndex((doc) => doc.documentId === infoDocumentId);
+    const documentIndex = documents.findIndex(
+      (doc) => doc.documentId === infoDocumentId
+    );
     if (documentIndex === -1 && !isLastSearchPageLoaded) {
       const scrollpane = document.getElementById('documentsScrollpane');
       if (!scrollpane) return;
       scrollpane.scrollTo({
         top: scrollpane.scrollHeight,
-      })
+      });
     } else if (documentIndex !== -1) {
-      const documentLine = document.getElementById(infoDocumentId)
+      const documentLine = document.getElementById(infoDocumentId);
       if (!documentLine) return;
-      documentLine.scrollIntoView({block: "end",})
+      documentLine.scrollIntoView({ block: 'end' });
       navigate(pathname + '#id=' + infoDocumentId);
     }
   }, [documents]);
@@ -234,7 +239,7 @@ export const DocumentsTable = (props: DocumentTableProps) => {
             ))}
           </tbody>
         </table>
-        {(loadingStatus === RequestStatus.pending && documents.length > 0)&& (
+        {loadingStatus === RequestStatus.pending && documents.length > 0 && (
           <div className="absolute bottom-0 w-full flex justify-center">
             <Spinner />
           </div>
