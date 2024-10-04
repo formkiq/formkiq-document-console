311f9792cf77926787f0f0c0f59891b11bab4cfa
AnastasiiaPirus <a.pirus2000@gmail.com>
2024-10-04 19:15:16 -0400

    WIP: Editing site

--- formkiq/apps/formkiq-document-console-react/src/app/Views/Account/sitesManagement.tsx
+++ formkiq/apps/formkiq-document-console-react/src/app/Views/Account/sitesManagement.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { Helmet } from 'react-helmet-async';
 import { useSelector } from 'react-redux';
-import { Settings } from '../../Components/Icons/icons';
+import { Close, Pencil, Settings } from '../../Components/Icons/icons';
 import { DocumentsService } from '../../helpers/services/documentsService';
 import { useAuthenticatedState } from '../../Store/reducers/auth';
 import { openDialog as openNotificationDialog } from '../../Store/reducers/globalNotificationControls';
@@ -45,10 +45,23 @@ function SitesManagement() {
     SiteGroupPermissions[]
   >([]);
   const [selectedGroupName, setSelectedGroupName] = useState<string>('');
+  const [selectedStatus, setSelectedStatus] = useState<
+    'ACTIVE' | 'INACTIVE' | ''
+  >('');
   const [siteGroups, setSiteGroups] = useState<string[]>([]);
+  const [siteTitle, setSiteTitle] = useState<string>('');
+  const [isEditingSiteTitle, setIsEditingSiteTitle] = useState<boolean>(false);
+  const [sites, setSites] = useState<any[]>([]);
+
+  function updateSites() {
+    DocumentsService.getSites().then((res) => {
+      if (res.status === 200) setSites(res.sites);
+    });
+  }
 
   useEffect(() => {
     dispatch(fetchGroups({ limit: 60 }));
+    updateSites();
   }, []);
 
   const handlePermissionChange = (
@@ -98,10 +111,6 @@ function SitesManagement() {
     });
   }, [editingSiteId]);
 
-  function cancelSiteEditing() {
-    setEditingSiteId(null);
-  }
-
   function saveSiteGroupPermissions() {
     if (!editingSiteId || !selectedGroupName) {
       return;
@@ -110,7 +119,6 @@ function SitesManagement() {
       permissions: editingPermissions,
     }).then((res) => {
       if (res.status === 200) {
-        setEditingSiteId(null);
         dispatch(
           openNotificationDialog({
             dialogTitle: 'Site group permissions have been saved.',
@@ -126,6 +134,32 @@ function SitesManagement() {
     });
   }
 
+  function saveSiteTitle() {
+    if (!editingSiteId || !siteTitle) {
+      return;
+    }
+    DocumentsService.updateSite(editingSiteId, {
+      site: { title: siteTitle },
+    }).then((res) => {
+      if (res.status === 200) {
+        setIsEditingSiteTitle(false);
+        setSiteTitle('');
+        dispatch(
+          openNotificationDialog({
+            dialogTitle: 'Site title has been saved.',
+          })
+        );
+      } else {
+        dispatch(
+          openNotificationDialog({
+            dialogTitle: 'Error. Site title has not been saved.',
+          })
+        );
+      }
+    });
+  }
+
+  console.log(sites);
   return (
     <>
       <Helmet>
@@ -141,8 +175,8 @@ function SitesManagement() {
           <div className=" flex flex-col w-full h-full px-6 mt-6">
             <h1 className="text-xl font-bold">Sites Management</h1>
             <ul className="flex flex-col gap-4 mt-4">
-              {user.sites.length > 0 &&
-                user.sites.map(
+              {sites.length > 0 ? (
+                sites.map(
                   (site: {
                     title: string;
                     siteId: string;
@@ -154,8 +188,42 @@ function SitesManagement() {
                     >
                       <div className="flex flex-row gap-8 justify-between items-center">
                         <div className="flex flex-col">
-                          <h1 className="text-lg font-bold">{site.title}</h1>
-                          <span className="text-xs text-gray-500 font-bold">
+                          {editingSiteId === site.siteId &&
+                          isEditingSiteTitle ? (
+                            <div className="flex gap-2 items-end h-10">
+                              <input
+                                type="text"
+                                className="w-full max-w-[250px] h-10 mt-4 rounded-md border border-gray-300 focus:ring-primary-500 focus:border-primary-500"
+                                value={siteTitle}
+                              />
+                              <ButtonTertiary
+                                onClick={() => setIsEditingSiteTitle(false)}
+                              >
+                                Cancel
+                              </ButtonTertiary>
+                              <ButtonPrimaryGradient onClick={saveSiteTitle}>
+                                Save
+                              </ButtonPrimaryGradient>
+                            </div>
+                          ) : (
+                            <h1 className="text-lg font-bold">
+                              {site.title}{' '}
+                              {editingSiteId === site.siteId && (
+                                <button
+                                  onClick={() => {
+                                    setIsEditingSiteTitle(true);
+                                    setSiteTitle(site.title);
+                                  }}
+                                  className="text-neutral-500 underline hover:text-primary-500 w-4 h-4"
+                                  title="Edit Site Name"
+                                >
+                                  <Pencil />
+                                </button>
+                              )}
+                            </h1>
+                          )}
+
+                          <span className="text-xs text-neutral-500 font-bold">
                             (siteId: {site.siteId})
                           </span>
                         </div>
@@ -163,13 +231,44 @@ function SitesManagement() {
                           <button
                             className="w-6 h-6 text-neutral-500 hover:text-neutral-900"
                             onClick={() => setEditingSiteId(site.siteId)}
+                            title="Edit Site Permissions"
                           >
                             <Settings />
+                          </button>
+                        )}
+                        {editingSiteId === site.siteId && (
+                          <button
+                            className="w-6 h-6 text-neutral-500 hover:text-neutral-900"
+                            onClick={() => setEditingSiteId(null)}
+                            title="Cancel Editing"
+                          >
+                            <Close />
                           </button>
                         )}
                       </div>
                       {editingSiteId === site.siteId && (
                         <>
+                          <h1 className="font-bold text-lg">Status</h1>
+                          <div className="flex gap-2 items-end h-10">
+                            <div className="w-1/2 max-w-[250px] h-10 mt-4">
+                              <RadioListbox
+                                selectedValue={selectedStatus}
+                                setSelectedValue={setSelectedStatus}
+                                values={['ACTIVE', 'INACTIVE']}
+                                titles={['Active', 'Inactive']}
+                                placeholderText="Status"
+                              />
+                            </div>
+                            <ButtonTertiary
+                              onClick={() => setIsEditingSiteTitle(false)}
+                            >
+                              Cancel
+                            </ButtonTertiary>
+                            <ButtonPrimaryGradient onClick={saveSiteTitle}>
+                              Save
+                            </ButtonPrimaryGradient>
+                          </div>
+                          <h1 className="font-bold text-lg">Site Groups</h1>
                           <div className="w-1/2 max-w-[250px] h-10 mt-4">
                             <RadioListbox
                               selectedValue={selectedGroupName}
@@ -197,7 +296,9 @@ function SitesManagement() {
                                 </label>
                               ))}
                               <div className="flex justify-end h-10 gap-2">
-                                <ButtonTertiary onClick={cancelSiteEditing}>
+                                <ButtonTertiary
+                                  onClick={() => setSelectedGroupName('')}
+                                >
                                   Cancel
                                 </ButtonTertiary>
                                 <ButtonPrimaryGradient
@@ -212,7 +313,10 @@ function SitesManagement() {
                       )}
                     </li>
                   )
-                )}
+                )
+              ) : (
+                <h3 className="text-center">There are no sites created yet.</h3>
+              )}
             </ul>
           </div>
         </div>
